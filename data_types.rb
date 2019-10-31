f7dc1c7d06f421e119343cf829d04264857d9c92
savithande <38084956+savithande@users.noreply.github.com>
2019-10-31 16:12:42 +0530

    Add files via upload

---
+++ data_types.rb
@@ -0,0 +1,45 @@
+name = "savitha\n" #string datatype
+print(name)
+
+#integer datatype
+age = 22
+puts(age)
+
+#floting datatype
+cgpa = 7.25
+puts(cgpa)
+
+#boolean value
+ismale = true
+puts(ismale)
+
+#null value
+istall = nil
+puts(istall)
+
+
+#String inbuilt methods
+
+sample = "Hey Beautiful\n"
+
+#to print the indivisual charecter
+print(sample[4])
+print("\n")
+
+#to convert whole string into uppercase latter"
+print (sample.upcase())
+
+#to convert whole string into lowercase latter
+print(sample.downcase())
+
+#to find the length of the string
+print(sample.length())
+print("\n")
+
+#to check the content present in the string
+print(sample.include? "Hey")
+print("\n")
+
+#to remove the extra spacess in the string
+sample = " \n     Hey Beautiful\n     "
+print(sample.strip())
---
+++ giraffe.rb
@@ -0,0 +1,13 @@
+#variable creation and assign the value into the variable and use that variable
+
+print " hello wold"
+character_name = "john"
+character_age = "26"
+puts ("there was a mon named " + character_name)
+puts ("he was " + character_age + " years old.")
+
+#same variable name but it contain the different values
+character_name = "savitha"
+character_age = "22"
+puts (" he really liked the name " + character_name)
+puts ("but didn't like beign " + character_age)
