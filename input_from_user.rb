e7632b4a90a38f52fb9721471bc094af341980be
savithande <38084956+savithande@users.noreply.github.com>
2019-11-02 11:42:24 +0530

    Add files via upload

---
+++ input_from_user.rb
@@ -0,0 +1,13 @@
+#take input from the user
+ puts("enter your name : ")
+ name1 = gets.chomp() #get method used to get the specific data
+ puts("Hello " + name1 + ", nice to meet you")
+ puts("what is your name: ")
+ name2 = gets.chomp() #chomp() method is used to avoid the unnessesary to go the next line
+ puts("hii," + name2 +", nice to meet you too")
+ puts("where are from " + name1 )
+ place1 = gets
+ puts("im from" + place1)
+ puts("where are u from " +name2)
+ place2 = gets
+ puts("im from " + place2)
