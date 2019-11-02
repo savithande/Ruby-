084c2123a79a117a919fa5c34a510613a2308fb9
savithande <38084956+savithande@users.noreply.github.com>
2019-11-02 14:29:21 +0530

    Add files via upload

---
+++ mad_lib_game.rb
@@ -0,0 +1,34 @@
+#mad_lib_game
+puts("THE {__animal_name1__} AND THE {__animal_name2__}")
+
+puts("Short Story for Kids written by: {__authour_name__}")
+
+puts("Once upon a time there was a little {__animal_name2__} living in {__place_name__}")
+
+puts("Suddenly, he saw a {__animal_name1__} , and the little {__animal_name2__} ran away as far as he could because he was so scared.")
+
+puts("The {__animal_name1__} followed the __animal_name2__ for several days but was unable to catch him.")
+
+puts("\n\n")
+
+puts("Enter first animal name: ")
+animal_name1 = gets.chomp()
+puts("Enter Secound animal name: ")
+animal_name2 = gets.chomp()
+puts("Enter authour name: ")
+authour_name = gets.chomp()
+puts("Enter place name: ")
+place_name = gets.chomp()
+
+puts("\n\n")
+
+
+puts("THE " + animal_name1 +" AND THE "+ animal_name2)
+
+puts("Short Story for Kids written by: "+ authour_name)
+
+puts("Once upon a time there was a little "+ animal_name2 + " living in " + place_name)
+
+puts("Suddenly, he saw a " + animal_name1 +" , and the little " + animal_name2 + "ran away as far as he could because he was so scared.")
+
+puts("The " + animal_name1 + " followed the " + animal_name2 + " for several days but was unable to catch him.")
