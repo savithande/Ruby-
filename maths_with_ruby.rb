0d8c8c87a66c25b3708cdac16b6fc9268d8ffaba
savithande <38084956+savithande@users.noreply.github.com>
2019-11-02 10:54:32 +0530

    Add files via upload

---
+++ maths_with_ruby.rb
@@ -0,0 +1,34 @@
+#addition of 2 numbers
+puts 5 + 4
+
+#multiplication of 2 numbers
+puts 3*234
+
+#division of 2 numbers
+puts 10/2
+
+#modulous operator
+puts 10%3
+
+#==========methods=============
+
+#absolute method
+num = -50.56
+puts(num.abs())
+
+#round method
+puts (num.round())
+
+#ceiling method
+puts(num.ceil())
+
+#floor method
+puts(num.floor())
+
+#===================math operations============
+
+#squir root operation
+puts(Math.sqrt(169))
+
+#log operation
+puts(Math.log(2))
