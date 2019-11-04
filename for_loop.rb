16308f20134c9a4855553fae7611bdd4ef504373
savithande <38084956+savithande@users.noreply.github.com>
2019-11-04 18:11:31 +0530

    Add files via upload

---
+++ for_loop.rb
@@ -0,0 +1,20 @@
+#for loop ....Syntax for the for loop is
+#for (element) in (range or condition)
+#-----statements1----
+#end
+
+animals = ["Tiger","Loin","Elephant","Chetha","bear"]
+
+for animal in animals
+  puts(animal)
+end
+
+puts("\n")
+for animal in animals
+  puts(animal[1])
+end
+
+puts("\n")
+for i in (0..5)
+  puts(i)
+end
